4e419c4e6928733b922fa4a54db53f92ceeee389
Wgexe <nik.sluzhaev@gmail.com>
2026-03-28 18:22:48 +0400

    1

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world!")
 }
+
+func (h *UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
+
+type UserHandler struct {
+	DB     string // В реальном коде здесь будет *sql.DB
+	Logger string
+}
+
+123123
